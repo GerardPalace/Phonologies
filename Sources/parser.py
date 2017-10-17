9ea61956f5c8a4ef33154768e51299a35194b533
GerardPalace <colin.pimmel@A1405-07.peda.pdb.univ-lille3.fr>
2017-10-17 15:00:39 +0200

    Ajout d'une barre de progression dans le parser

--- Sources/parser.py
+++ Sources/parser.py
@@ -2,21 +2,34 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import math
 
 #Ce programme a pour but d'écrire un fichier décrivant les colonnes et les différentes valeurs possible
+
+def progressbar(ratio):
+    percent = int(ratio*100)
+    progress = int(ratio*10)
+    strbar = "[" + "#"*progress + " "*(10-progress)+ "]" + str(percent) + "%"
+    print("\r"*len(strbar) + strbar, end='', flush='True')
 
 df = pd.read_table("../Data/language.csv", ',')
 
 modal_value_file = open("modal_value", "w")
+total = len(df.columns)
+current = 0
 for c in df.columns.values:
+    progressbar(current/total)
+    current+=1
     possible_values = []
     str_values = ""
     #On retire les valeurs trop uniques ou quantitatives
     if (c!="latitude" and c!="longitude" and c!="wals_code" \
     and c!="iso_code" and c!="glottocode" and c!="Name" and c!="countrycodes"):
         for i in range(len(df)):
-            if (df[c][i]!=None and df[c][i] not in possible_values):
+            if (pd.isnull(df.loc[i, c])==False and df[c][i] not in possible_values):
                 possible_values.append(df[c][i])
                 str_values += str(df[c][i]) + " ; "
         if (str_values!=""):
             modal_value_file.write(c + " : " + str_values + "\n\n")
+progressbar(1)
+print("")
