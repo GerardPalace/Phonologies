befd4a75565ad3f4c9bfbcae0bd838cb4458536c
GerardPalace <pimmel.colin59@gmail.com>
2017-11-02 23:48:25 +0100

    Ajout des fichiers html

---
+++ Sources/Map/consonant_map.js
@@ -0,0 +1,56 @@
+var mymap = L.map('mapid').setView([15, 10], 2);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+{
+  id: 'mapbox.streets-basic',
+  minZoom: 2,
+  maxZoom: 6,
+}).addTo(mymap);
+
+function onEachFeature(feature, layer) {
+  if (feature.properties && feature.properties) {
+    layer.bindPopup("Langue : " + feature.properties.name + "<br> Consonant Inventory : " + feature.properties.description1);
+  }
+}
+
+function getColor(d) {
+    return d == 1  ? '#f1eef6' :
+           d == 2  ? '#d0d1e6' :
+           d == 3  ? '#a6bddb' :
+           d == 4  ? '#74a9cf' :
+           d == 5  ? '#2b8cbe' :
+                     '#045a8d'
+}
+
+function pointToLayer(feature, latlng) {
+  return L.circleMarker(latlng, {
+    radius: 6,
+    fillColor: getColor(feature.properties.valueNumber1),
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 1
+  });
+}
+
+L.geoJson(coordinates, { onEachFeature: onEachFeature, pointToLayer: pointToLayer
+}).addTo(mymap);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function(mymap)
+{
+  var div = L.DomUtil.create('div', 'info legend'),
+  grades = [1, 2, 3, 4, 5]
+  labels = ["Small", "Moderatly small", "Average", "Moderately large", "Large"];
+
+  // loop through our density intervals and generate a label with a colored square for each interval
+  div.innerHTML += "<h4>Consonant Inventory</h4>"
+  for (var i = 0; i < grades.length; i++)
+  {
+    div.innerHTML += '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+                      labels[i] + '<br>';
+  }
+  return div;
+};
+
+legend.addTo(mymap);
